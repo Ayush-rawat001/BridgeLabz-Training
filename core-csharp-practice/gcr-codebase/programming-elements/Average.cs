b6b3492bde8a48eee73bb822b4879d8ea7b6652b
gla0450-ayushrawat <ayushrawat200511@gmail.com>
2025-12-18 16:44:08 +0530

    [Ayush Rawat] add:10 coding problems

---
+++ core-csharp-practice/gcr-codebase/programming-elements/Average.cs
@@ -0,0 +1,9 @@
+using System;
+class Average{
+static void Main(){
+ double num1=double.Parse(Console.ReadLine());
+ double num2=double.Parse(Console.ReadLine());
+ double num3=double.Parse(Console.ReadLine());
+double avg=(num1+num2+num3)/3;
+ Console.WriteLine(avg);
+}}
